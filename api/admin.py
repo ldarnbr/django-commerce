c9a6564b31e24b7b3231feb94eb5d95b77506ea2
Laurence Brown <ldarnbr@gamil.com>
2026-07-28 22:16:34 +0100

    feat: added ModelAdmin classes to the admin panel

--- api/admin.py
+++ api/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
 from .models import Item, ShoppingBasket, BasketItem, Order, OrderItem
 # https://docs.djangoproject.com/en/6.0/intro/tutorial07/
+# Adjusted to present lists in admin view showing relevant information for each db table.
 # Register your models here.
 
-admin.site.register(Item)
-admin.site.register(ShoppingBasket)
-admin.site.register(BasketItem)
-admin.site.register(Order)
-admin.site.register(OrderItem)
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'stock_count', 'sale_discount')
+
+@admin.register(ShoppingBasket)
+class ShoppingBasketAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer')
+
+@admin.register(BasketItem)
+class BasketItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'basket', 'item', 'quantity')
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer')
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'item', 'quantity', 'price')
